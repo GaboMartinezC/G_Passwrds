e18cf5b712f5cb3e8bd81094a59e4051af4f04a6
Gabriel de Jesús <martinezcgabriel24@gmail.com>
2023-01-14 18:49:06 -0600

    Ojalá me lleve el diablo

--- src/BL/UsuarioBL.java
+++ src/BL/UsuarioBL.java
@@ -1,6 +1,33 @@
 package BL;
+import DAL.*;
+import Entidades.*;
+import java.util.ArrayList;
 
 public class UsuarioBL 
 {
-    
+    public boolean Login (Usuario usuario, String contraIng) throws Exception
+    {
+        UsuarioDAL dal = new UsuarioDAL();
+        return dal.Login(usuario, contraIng);
+    }
+    public Usuario Buscar(String descripcion) throws Exception
+    {
+        UsuarioDAL dal = new UsuarioDAL();
+        return dal.Buscar(descripcion);
+    }
+    public ArrayList<Usuario> BuscarTodos() throws Exception
+    {
+        UsuarioDAL dal = new UsuarioDAL();
+        return dal.BuscarTodos();
+    }
+    public void Borrar (Usuario usuario) throws Exception
+    {
+       UsuarioDAL dal = new UsuarioDAL();
+       dal.Borrar(usuario);
+    }
+    public boolean Ingresar(Usuario usuario) throws Exception
+    {
+        UsuarioDAL dal = new UsuarioDAL();
+        return dal.Ingresar(usuario);
+    }
 }
